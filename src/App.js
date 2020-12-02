738467f3e1dacc01d70f8cb9f7083527825ba389
Rama <you@example.com>
2020-12-02 17:39:56 +0530

    1 Hour mark

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import Jumbrotron from './components/jumbotron'
+import jumboData from './fixtures/jumbo.json'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Jumbrotron.container>
+     {jumboData.map((item)=>(
+       <Jumbrotron key ={item.id} direction={item.direction}>
+         <p>{item.title}</p>
+         <p>{item.subTitle}</p>
+         <p>{item.image}</p>
+         <p>{item.alt}</p> 
+       </Jumbrotron>
+     )
+     )}
+   </Jumbrotron.container>
   );
 }
 
-export default App;
